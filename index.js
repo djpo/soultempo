c910ad4d8bba8d70d97f494177ef319a8fee9560
djpo <derekpoppe@gmail.com>
2015-12-15 19:47:22 -0800

    successfully requested song id from EN API

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var app = express();
-// var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var request = require('request');
+var bodyParser = require('body-parser');
 // var db = require('./models');
+var app = express();
 
 app.set('view engine', 'ejs');
-
-// app.use(bodyParser.urlencoded({extended: false}));
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/static'));
 
 var homepage = require('./controllers/index.js');
