feffa647611525112d4ebb090dec8dd4da76b1c0
djpo <derekpoppe@gmail.com>
2016-05-24 17:52:20 -0700

    Refactor controllers/results.js. Update package.json with new github project name.

--- controllers/results.js
+++ controllers/results.js
@@ -6,24 +6,26 @@ var request = require('request');
 
 router.post('/', function(req, res) {
 	var links = [];
+	var echoNestUrlBeg = 'http://developer.echonest.com/api/v4/song/search?api_key=';
 	if (!req.body.artist_0 && !req.body.title_0) {
 		console.log('1st search has no artist nor title');
 	} else {
-		links.push('http://developer.echonest.com/api/v4/song/search?api_key=' + process.env.EN_API_KEY + '&artist=' + req.body.artist_0 + '&title=' + req.body.title_0);
+		links.push(echoNestUrlBeg + process.env.EN_API_KEY + '&artist=' + req.body.artist_0 + '&title=' + req.body.title_0);
 	}
 	if (!req.body.artist_1 && !req.body.title_1) {
 		console.log('2nd search field has no artist nor title');
 	} else {
-		links.push('http://developer.echonest.com/api/v4/song/search?api_key=' + process.env.EN_API_KEY + '&artist=' + req.body.artist_1 + '&title=' + req.body.title_1);
+		links.push(echoNestUrlBeg + process.env.EN_API_KEY + '&artist=' + req.body.artist_1 + '&title=' + req.body.title_1);
 	}
 	if (!req.body.artist_2 && !req.body.title_2) {
 		console.log('3rd search field has no artist nor title');
 	} else {
-		links.push('http://developer.echonest.com/api/v4/song/search?api_key=' + process.env.EN_API_KEY + '&artist=' + req.body.artist_2 + '&title=' + req.body.title_2);
+		links.push(echoNestUrlBeg + process.env.EN_API_KEY + '&artist=' + req.body.artist_2 + '&title=' + req.body.title_2);
 	}
 
 //////////////////// API REQUEST
 	var getData = function(url, cb) {
+		console.log('EN API search initiated...');
 		request(url, function(err, response, body) {
 			var fullResponseBody = JSON.parse(body);
 			// if response contains ≥ 1 song, add to foundSongs
@@ -46,14 +48,8 @@ router.post('/', function(req, res) {
 		});
 	};
 
-	console.log('____________________');
-	console.log('EN API search initiated...');
-	async.concat(links, getData, function(err, foundSongs) {
-		console.log('EN API search complete. Songs found:');
-		console.log('____________________');
-		console.log(foundSongs);
-		console.log('_______(end)________');
 
+	async.concat(links, getData, function(err, foundSongs) {
 		res.render('results', {foundSongs: foundSongs});
 	});
 //////////////////// API REQUEST DONE
