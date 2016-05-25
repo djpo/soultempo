8512dfae98f53194d3348d986be1f362657b7798
djpo <derekpoppe@gmail.com>
2016-05-24 18:12:07 -0700

    Refactor: convert all tabs to (2) spaces.

--- controllers/process.js
+++ controllers/process.js
@@ -6,65 +6,65 @@ var router = express.Router();
 
 
 router.post('/', function(req, res) {
-	console.log('_____process.js POST route_____');
-	var procIdArray = req.body.idsToPass;
+  console.log('_____process.js POST route_____');
+  var procIdArray = req.body.idsToPass;
 
 //////////
-	var links = [];
- 	procIdArray.forEach(function(songId) {
- 		links.push('http://developer.echonest.com/api/v4/song/profile?api_key=' + process.env.EN_API_KEY + '&id=' + songId + '&bucket=audio_summary');
- 	});
- 	console.log(links);
+  var links = [];
+  procIdArray.forEach(function(songId) {
+    links.push('http://developer.echonest.com/api/v4/song/profile?api_key=' + process.env.EN_API_KEY + '&id=' + songId + '&bucket=audio_summary');
+  });
+  console.log(links);
 
-	var getData = function(url, cb){
-		request(url, function(err, response, body) {
-			var fullResponseBody = JSON.parse(body);
-			// if response contains ≥ 1 song, add to foundSongs
-			if (!err && response.statusCode === 200 && fullResponseBody.response.songs.length > 0) {
-				var returnedSong = fullResponseBody.response.songs[0];
-				var addedSong = {
-					id: returnedSong.id,
-					artist: returnedSong.artist_name,
-					title: returnedSong.title,
-					tempo: returnedSong.audio_summary.tempo
-				};
-			} else if (err) {
-				console.log(err);
-			} else if (response.statusCode !== 200) {
-				console.log('statusCode: ' + response.statusCode);
-			} else if (fullResponseBody.response.songs.length <= 0) {
-				console.log('no song returned');
-			}
-			// return foundSong, for use in async.concat cb below
-			cb(null, addedSong);
-		});
-	};
+  var getData = function(url, cb){
+    request(url, function(err, response, body) {
+      var fullResponseBody = JSON.parse(body);
+      // if response contains ≥ 1 song, add to foundSongs
+      if (!err && response.statusCode === 200 && fullResponseBody.response.songs.length > 0) {
+        var returnedSong = fullResponseBody.response.songs[0];
+        var addedSong = {
+          id: returnedSong.id,
+          artist: returnedSong.artist_name,
+          title: returnedSong.title,
+          tempo: returnedSong.audio_summary.tempo
+        };
+      } else if (err) {
+        console.log(err);
+      } else if (response.statusCode !== 200) {
+        console.log('statusCode: ' + response.statusCode);
+      } else if (fullResponseBody.response.songs.length <= 0) {
+        console.log('no song returned');
+      }
+      // return foundSong, for use in async.concat cb below
+      cb(null, addedSong);
+    });
+  };
 
-	console.log('____________________');
-	console.log('EN API calls initiated...');
-	async.concat(links, getData, function(err, gatheredSongs) {
-		console.log('____________________');
-		console.log('EN API calls returned:');
+  console.log('____________________');
+  console.log('EN API calls initiated...');
+  async.concat(links, getData, function(err, gatheredSongs) {
+    console.log('____________________');
+    console.log('EN API calls returned:');
 
-		gatheredSongs.forEach(function(song) {
-			db.favorite.findOrCreate({
-				where: {
-					u_id: req.session.user,
-					song_id: song.id,
-					artist: song.artist,
-					title: song.title,
-					tempo: song.tempo
-				}
-			}).spread(function() {
-				console.log('____________________');
-				console.log("added '" + song.title + "' to favorites table");
-			});
-		});
-		console.log('_______(async end)________');
-	});
+    gatheredSongs.forEach(function(song) {
+      db.favorite.findOrCreate({
+        where: {
+          u_id: req.session.user,
+          song_id: song.id,
+          artist: song.artist,
+          title: song.title,
+          tempo: song.tempo
+        }
+      }).spread(function() {
+        console.log('____________________');
+        console.log("added '" + song.title + "' to favorites table");
+      });
+    });
+    console.log('_______(async end)________');
+  });
 //////////
-	console.log('_______right before res.status(200)________');
-	res.status(200).send('TestStuff');
+  console.log('_______right before res.status(200)________');
+  res.status(200).send('TestStuff');
 });
 
 
--- controllers/summary.js
+++ controllers/summary.js
@@ -4,52 +4,52 @@ var db = require('./../models');
 var session = require('express-session');
 
 function calcSoulTempo(songs) {
-	var tempoNumerator = 0;
-	songs.forEach(function (song) {
-		tempoNumerator += song.dataValues.tempo;
-	});
-	return Math.round(tempoNumerator / songs.length);
+  var tempoNumerator = 0;
+  songs.forEach(function (song) {
+    tempoNumerator += song.dataValues.tempo;
+  });
+  return Math.round(tempoNumerator / songs.length);
 };
 
 
 router.all('/', function (req, res, next) {
-	if (req.currentUser) {
-		next();
-	} else {
-		req.flash('danger','You must log in before viewing your collection.');
-		res.redirect('/');
-	}
+  if (req.currentUser) {
+    next();
+  } else {
+    req.flash('danger','You must log in before viewing your collection.');
+    res.redirect('/');
+  }
 });
 
 router.get('/', function(req, res) {
-	console.log('_____summary.js GET route_____');
+  console.log('_____summary.js GET route_____');
 
-	db.favorite.findAll({
-		where: {u_id: req.session.user}
-		// order: 'updatedAt DESC'
-	}).then(function(favorites) {
+  db.favorite.findAll({
+    where: {u_id: req.session.user}
+    // order: 'updatedAt DESC'
+  }).then(function(favorites) {
 
-		console.log('---favorites.length: ' + favorites.length);
+    console.log('---favorites.length: ' + favorites.length);
 
 
-			var st = calcSoulTempo(favorites) || -1;
+      var st = calcSoulTempo(favorites) || -1;
 
-			db.user.update({
-				soul_tempo: st
-			}, {
-				where: {
-					id: req.session.user
-				}
-			}).then(function() {
-				console.log('updated user...');
-			});
-			
-		// }
+      db.user.update({
+        soul_tempo: st
+      }, {
+        where: {
+          id: req.session.user
+        }
+      }).then(function() {
+        console.log('updated user...');
+      });
+      
+    // }
 
-		console.log('st: ' + st);
-		console.log('typeof st: ' + typeof st);
-		res.render('summary', {collection: favorites, st: st});
-	});
+    console.log('st: ' + st);
+    console.log('typeof st: ' + typeof st);
+    res.render('summary', {collection: favorites, st: st});
+  });
 });
 
 
